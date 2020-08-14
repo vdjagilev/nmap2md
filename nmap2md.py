b5a796d8ebd684988f51c5dac0c6ab3a0004074a
vdjagilev <2762286+vdjagilev@users.noreply.github.com>
2020-08-15 01:53:25 +0300

    Version bump & gitignore

--- nmap2md.py
+++ nmap2md.py
@@ -7,7 +7,7 @@ from optparse import OptionParser
 
 import columns_definition
 
-__version__ = "1.1.0"
+__version__ = "1.1.2"
 
 parser = OptionParser(usage="%prog [options] file.xml", version="%prog " + __version__)
 
